d16491f9cd3bed7d04b7292da6cfa3220972b123
Heather Goldsby <heather.goldsby@gmail.com>
2015-03-25 14:42:25 -0400

    propagule viability
    Added support for a propagule size death-birth model at the multicell
    level.

---
+++ src/subpopulation_propagule_split.h
@@ -0,0 +1,90 @@
+/* subpopulation_propagule_plane.h
+ *
+ * This file is part of EALib.
+ *
+ * Copyright 2014 David B. Knoester, Heather J. Goldsby.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _SUBPOPULATION_PROPAGULE_SPLIT_H_
+#define _SUBPOPULATION_PROPAGULE_SPLIT_H_
+
+#include <algorithm>
+#include <utility>
+
+#include <ea/metadata.h>
+#include "stripes.h"
+
+using namespace ealib;
+
+
+/*! This recombination operator is metapopulation-specific; it copies a
+ propagule from a parent subpopulation, mutates it, and adds it to an
+ offspring subpopulation.
+ 
+ Cells are flagged as propagules using the create_propagule and deploy_propagule methods.
+ */
+struct subpopulation_propagule_split {
+    std::size_t capacity() const { return 1; }
+    
+    template <typename Population, typename MEA>
+    void operator()(Population& parents, Population& offspring, MEA& mea) {
+    
+        double num_prop = ceil(get<PROP_COUNT>(*(parents[0]),0) / 2.0);
+        
+        //if (num_prop == 0) return;
+
+        // get a new subpopulation:
+        typename MEA::individual_ptr_type p = mea.make_individual();
+        p->initialize(mea.md());
+        p->reset_rng(mea.rng().seed());
+        
+        
+        typedef typename MEA::subpopulation_type::population_type propagule_type;
+        // shuffle the population
+        std::random_shuffle(parents[0]->population().begin(), parents[0]->population().end(), parents[0]->rng());
+                
+        int num_moved = 0;
+        for(typename propagule_type::iterator j=parents[0]->population().begin(); j!=parents[0]->population().end(); ++j) {
+            if ((get<IS_PROPAGULE>(**j, 0) == 2) && (*j)->alive()) {
+                typename MEA::subpopulation_type::genome_type r((*j)->genome().begin(),
+                                                                (*j)->genome().begin()+(*j)->hw().original_size());
+                typename MEA::subpopulation_type::individual_ptr_type q = p->make_individual(r);
+                
+                inherits_from(**j, *q, *p);
+                
+                
+                const position_type pos = (*j)->position();
+                
+                
+                //iterator insert_at(iterator i, individual_ptr_type x, const position_type& pos) {
+                p->insert_at(p->end(), q, pos);
+                
+                (*j)->alive() = false;
+                parents[0]->events().death(**j,*(parents[0]));
+                
+                
+                ++num_moved;
+                
+            }
+            if (num_moved == num_prop) {
+                break;
+            }
+        }
+        
+        offspring.insert(offspring.end(),p);
+    }
+};
+
+#endif
