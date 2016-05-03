fd1b8bfe428495a51a03c10ff5a24e71a6d649f0
Heather Goldsby <heather.goldsby@gmail.com>
2016-05-03 15:22:52 -0400

    .

--- src/propagule.h
+++ src/propagule.h
@@ -77,11 +77,11 @@ namespace ealib {
                     prop_count = 0;
                     for(typename propagule_type::iterator j=i->population().begin(); j!=i->population().end(); ++j) {
                         if ((*j)->alive()) {
-                            if (get<IS_PROPAGULE>(**j,0) == 1) {
+                            if (get<IS_PROPAGULE>(*i) == 1) {
                                 prop_count++;
                                 
                             }
-                            subpop_prop_size(get<PROPAGULE_SIZE>(**j, get<START_PROPAGULE_SIZE>(**j, 0)));
+                            subpop_prop_size(get<PROPAGULE_SIZE>(**j, get<START_PROPAGULE_SIZE>(**j)));
 
                         }
                         
