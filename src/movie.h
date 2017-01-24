16cb31ef4f911186486f106ed6334ba78eb854c5
Heather Goldsby <heather.goldsby@gmail.com>
2017-01-24 14:10:55 -0500

    movie update

--- src/movie.h
+++ src/movie.h
@@ -20,6 +20,128 @@
 namespace ealib {
     namespace analysis {
         
+        LIBEA_ANALYSIS_TOOL(movie_for_competitions_spatial) {
+            
+            int update_max = get<METAPOP_COMPETITION_PERIOD>(ea);
+            int max_fit = 0;
+            typename EA::individual_type best;
+            
+            for(typename EA::iterator i=ea.begin(); i!=ea.end(); ++i) {
+                
+                // not preserving location
+                typename EA::individual_ptr_type p = ea.make_individual();
+                p->initialize(ea.md());
+                //p->reset_rn(ea.rng().seed());
+                
+                //typename EA::individual_type tmp(*i->traits().founder());
+                typedef typename EA::subpopulation_type::population_type propagule_type;
+                for (typename propagule_type::iterator j=(*i->traits().founder()).population().begin(); j!= (*i->traits().founder()).population().end(); ++j) {
+                    int pos = 0;
+                    typename EA::subpopulation_type::genome_type r((*j)->genome().begin(),
+                                                                    (*j)->genome().begin()+(*j)->hw().original_size());
+                    typename EA::subpopulation_type::individual_ptr_type q = p->make_individual(r);
+
+                    
+
+                    p->insert_at(p->end(),q, p->env().location((*j)->position()).position());
+
+                }
+                
+                
+                for (int j=0; j<=update_max; ++j) {
+                    p->update();
+                }
+                recalculate_fitness(*p, ea);
+                
+                
+                
+                
+                // Calc fitness for each subpop
+                //                eval_two_stripes(tmp);
+                
+                // copy the stripe fit to the accumulator and also the subpop
+                double sf =get<STRIPE_FIT>(*p);
+                
+                if (sf > max_fit) {
+                    max_fit = sf;
+                    best = *i;
+                }
+            }
+            
+            
+            datafile df("movie.dat");
+            df.write(get<SPATIAL_X>(ea));
+            df.write(get<SPATIAL_Y>(ea));
+            df.endl();
+            
+            //typename EA::individual_type best_founder = *best.traits().founder();
+            // not preserving location
+            typename EA::individual_ptr_type best_founder = ea.make_individual();
+            best_founder->initialize(ea.md());
+            //p->reset_rn(ea.rng().seed());
+            
+            //typename EA::individual_type tmp(*i->traits().founder());
+            typedef typename EA::subpopulation_type::population_type propagule_type;
+            for (typename propagule_type::iterator j=(*best.traits().founder()).population().begin(); j!= (*best.traits().founder()).population().end(); ++j) {
+                int pos = 0;
+                typename EA::subpopulation_type::genome_type r((*j)->genome().begin(),
+                                                               (*j)->genome().begin()+(*j)->hw().original_size());
+                typename EA::subpopulation_type::individual_ptr_type q = best_founder->make_individual(r);
+                
+                
+                
+                best_founder->insert_at(best_founder->end(),q, best_founder->env().location((*j)->position()).position());
+                
+            }
+
+            
+            for (int j=0; j<=update_max; ++j) {
+                best_founder->update();
+                df.write(j);
+                // grab info based on location...
+                for (int x=0; x < get<SPATIAL_X>(ea); ++x) {
+                    for (int y=0; y<get<SPATIAL_Y>(ea); ++y){
+                        
+                        //typename EA::individual_type::environment_type::location_type* l = &best_founder.traits().founder()->env().location(x,y);
+                        typename EA::individual_type::environment_type::location_type* l = &best_founder->env().location(x,y);
+                        
+                        
+                        if (l->occupied()) {
+                            std::string lt = get<LAST_TASK>(*(l->inhabitant()),"");
+                            
+                            if(lt == "not") {
+                                df.write("1");
+                            }
+                            if (lt == "nand") {
+                                df.write("2");
+                            }
+                            if (lt == "or") {
+                                df.write("3");
+                            }
+                            if (lt == "ornot") {
+                                df.write("4");
+                            }
+                            if (lt == "") {
+                                df.write("0");
+                            }
+                            
+                            
+                        } else {
+                            df.write("-1");
+                        }
+                        
+                    }
+                }
+                df.endl();
+                
+            }
+            
+            //df.endl();
+            
+        }
+        
+
+        
         /* Run through each multicell... 
          - reset it
          - run for competition period
